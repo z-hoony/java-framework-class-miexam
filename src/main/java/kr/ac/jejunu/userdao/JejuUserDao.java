ca5eadc0dd170faaad317377f00b19f3d115e965
jade <jade.h@kakaocorp.com>
2020-05-10 11:17:53 +0900

    중복 코드는 하나의 클래스를 상속받게하고 달라지는 부분은 abstract로 선언 (#2)

--- src/main/java/kr/ac/jejunu/userdao/JejuUserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/JejuUserDao.java
@@ -2,49 +2,8 @@ package kr.ac.jejunu.userdao;
 
 import java.sql.*;
 
-public class JejuUserDao {
-    public User get(Integer id) throws ClassNotFoundException, SQLException {
-        Connection connection = getConnection();
-        PreparedStatement preparedStatement =
-                connection.prepareStatement("select * from userinfo where id = ?");
-        preparedStatement.setLong(1, id);
-
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-
-        User user = new User();
-        user.setId(resultSet.getInt("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
-        //리턴
-        return user;
-    }
-
-    public Integer insert(String name, String password) throws ClassNotFoundException, SQLException {
-        Connection connection = getConnection();
-        PreparedStatement preparedStatement =
-                connection.prepareStatement("insert into userinfo (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
-        preparedStatement.setString(1, name);
-        preparedStatement.setString(2, password);
-        preparedStatement.execute();
-
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        resultSet.next();
-
-        Integer id = resultSet.getInt(1);
-
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
-
-        return id;
-    }
-
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
+public class JejuUserDao extends UserDao {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=UTC"
                 , "jeju", "jejupw");
--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -2,6 +2,47 @@ package kr.ac.jejunu.userdao;
 
 import java.sql.*;
 
-public class UserDao {
+abstract public class UserDao {
+    public User get(Integer id) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select * from userinfo where id = ?");
+        preparedStatement.setLong(1, id);
 
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        //리턴
+        return user;
+    }
+
+    public Integer insert(String name, String password) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into userinfo (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, password);
+        preparedStatement.execute();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+
+        Integer id = resultSet.getInt(1);
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+    }
+
+    abstract public Connection getConnection() throws ClassNotFoundException, SQLException;
 }
