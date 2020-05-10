3d79f04c35df4054dcd45ef654bf4d08d76c0061
jade <jade.h@kakaocorp.com>
2020-05-10 10:44:56 +0900

    사용자 생성 테스트 케이스 작성 (#1)

--- src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
@@ -19,5 +20,14 @@ public class UserDaoTests {
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
+    }
+
+    public void testInsert() {
+        UserDao userDao = new UserDao();
+        String name = "jade";
+        String password = "1234";
+        User createdUser = userDao.insert(name, password);
+
+        assertThat(createdUser.getId(), greaterThan(0));
     }
 }
