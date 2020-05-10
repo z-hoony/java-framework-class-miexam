c70af5e7c1d81c4812243c37c9f88972f75645fa
hoony <hoony@hoony.org>
2020-05-10 14:44:14 +0900

    JdbcTemplate 이용 (#13)

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -1,35 +1,57 @@
 package kr.ac.jejunu.userdao;
 
-import java.sql.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class UserDao {
-    private final JdbcContext jdbcContext;
+    private final JdbcTemplate jdbcContext;
 
-    UserDao(JdbcContext jdbcContext) {
+    UserDao(JdbcTemplate jdbcContext) {
         this.jdbcContext = jdbcContext;
     }
 
-    public User get(Integer id) throws SQLException {
-        Object[] objects = { id };
+    public User get(Integer id) {
+        Object[] params = { id };
         String sql = "select * from userinfo where id = ?";
-        return jdbcContext.get(sql, objects);
+        return jdbcContext.query(sql, params, rs -> {
+            User user = null;
+            if (rs.next()) {
+                user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+            }
+            return user;
+        });
     }
 
-    public Integer insert(String name, String password) throws SQLException {
-        Object[] objects = { name, password };
+    public Integer insert(String name, String password) {
+        Object[] params = { name, password };
         String sql = "insert into userinfo (name, password) values (?, ?)";
-        return jdbcContext.insert(sql, objects);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcContext.update(con -> {
+            PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+            return preparedStatement;
+        }, keyHolder);
+        return keyHolder.getKey().intValue();
     }
 
-    public void update(User user) throws SQLException {
-        Object[] objects = { user.getName(), user.getPassword(), user.getId() };
+    public void update(User user) {
+        Object[] params = { user.getName(), user.getPassword(), user.getId() };
         String sql = "update userinfo set name = ?, password = ? where id = ?";
-        jdbcContext.update(sql, objects);
+        jdbcContext.update(sql, params);
     }
 
-    public void delete(Integer id) throws SQLException {
-        Object[] objects = { id };
+    public void delete(Integer id) {
+        Object[] params = { id };
         String sql = "delete from userinfo where id = ?";
-        jdbcContext.update(sql, objects);
+        jdbcContext.update(sql, params);
     }
 }
